0690354ff5bd83e9a570f0ecf1deed51eec0a7e9
Anthony YOU <spleen85@proton.me>
2025-06-05 15:22:12 +0200

    Rethinking way to manage app

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-import argparse
-from controllers.base import Controller
-from views.prompt import Prompt
-from views.show import Show
-from controllers.mysql import Mysql
-from controllers.models import EpicUser
-from controllers.authentication import Authentication
-
-
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('login', nargs='?', default=None, help="Email needed as login")
-    args = parser.parse_args()
-    app = Controller(Prompt, Show, Mysql, Authentication)
-    user = EpicUser()
-    user.email = args.login
-    app.start(user)
+    return True
 
 
 if __name__ == "__main__":
