c87aa607da887c2b8462cdf537b9517893f725f0
Anthony YOU <spleen85@proton.me>
2025-06-23 10:10:17 +0200

    Add sentry

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ from views.show import Show
 from controllers.db import Mysql
 from controllers.authentication import Authentication
 from controllers.session import Session
+import sentry_sdk
 
 locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+
+sentry_sdk.init(
+    dsn="https://658b44973063f1d61a14d521cf60ae2d@o4509252956127232.ingest.de.sentry.io/4509546953375824",
+    # Add data like request headers and IP for users,
+    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+    send_default_pii=True,
+)
 
 
 def main():
