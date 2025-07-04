c9b2cf761ce38802a59642424cca243bf7a1d097
Anthony YOU <spleen85@proton.me>
2025-07-04 16:04:25 +0200

    Comment main.py

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ from controllers.authentication import Authentication
 from controllers.session import Session
 import sentry_sdk
 
+"Set date in french"
 locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
 
 sentry_sdk.init(
     dsn="https://658b44973063f1d61a14d521cf60ae2d@o4509252956127232.ingest.de.sentry.io/4509546953375824",
-    # Add data like request headers and IP for users,
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
     send_default_pii=True,
 )
 
 
 def main():
+    "Retrieve the email argument (if given) before launching the application"
     parser = argparse.ArgumentParser()
     parser.add_argument('login', nargs='?', default=None, help="Email needed as login")
     args = parser.parse_args()
